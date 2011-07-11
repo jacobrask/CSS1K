5fcec24151a4a0f0de46c59b7655f5b2a287702f
Jacob Rask <jacob@jacobrask.net>
2011-07-11 11:56:30 +0200

    Add Sass/Compass support and example scss theme

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Compass Configuration
+
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/styles'
+
+# File system locations
+sass_dir              = 'styles'
+css_dir               = 'styles'
+
+# Set to true for easier debugging
+line_comments         = false
+preferred_syntax      = :scss
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :compressed
